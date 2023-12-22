d7538ebfb0c2a4ba7eec3f99e439f37d106ddc5a
Ber Davoglio <berdavoglio@gmail.com>
2023-12-22 14:41:18 -0300

    Removed dist

--- dist/routes/User/jwt_routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _jwt_controller = require('../../controllers/User/jwt_controller'); var _jwt_controller2 = _interopRequireDefault(_jwt_controller);
-
-const router = new (0, _express.Router)();
-
-router.post('/', _jwt_controller2.default.store);
-
-exports. default = router;
