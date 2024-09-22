dc10b39edbf440b9f8baf493f525f8717704ed5e
Ber Davoglio <berdavoglio@gmail.com>
2024-09-22 11:51:58 -0300

    [VUEJS] Fix - Fixing Fourth feedback /3

--- front/src/components/home/HomeDayHospitalComponent.vue
+++ front/src/components/home/HomeDayHospitalComponent.vue
@@ -7,10 +7,10 @@
     </div>
     <div className="text-justify">
       <div className="my-2">
-        O Hospital dia, localizado na nossa unidade de São José dos Pinhais, foi criado para prestar atendimento aos pacientes que necessitam de uma assistência intermediária entre o ambulatório e um hospital integral e atende pacientes particulares e conveniados.
+        <span className="text-[0.1px] mr-8">.</span>O Hospital dia, localizado na nossa unidade de São José dos Pinhais, foi criado para prestar atendimento aos pacientes que necessitam de uma assistência intermediária entre o ambulatório e um hospital integral, atendemos particular e convênios.
       </div>
       <div className="my-2">
-        Contamos com uma competente equipe treinada para providenciar o melhor acolhimento para o paciente, com médicos, psicólogos, promovendo grupos terapêuticos e um espaço aconchegante de tratamento.
+        <span className="text-[0.1px] mr-8">.</span>Contamos com uma competente equipe treinada para providenciar o melhor acolhimento para o paciente, com médicos, psicólogos, promovendo grupos terapêuticos e um espaço aconchegante de tratamento.
       </div>
     </div>
   </div>
